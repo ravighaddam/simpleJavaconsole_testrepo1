702ea2da831971a666de96c1593cc339a73242c1
Ravi Ghaddam <ravighaddam1999@gmail.com>
2023-06-21 09:06:13 +0530

    updated Javaconsole.java file

--- src/simplepkg1/Javaconsole.java
+++ src/simplepkg1/Javaconsole.java
@@ -29,20 +29,20 @@ public class Javaconsole {
         
         /* Get the input of two variables */
         System.out.print("Enter the Two Numbers of users:");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
         /*
          * Call Function addition_two_numbers, subtraction_two_numbers,
          * multiplication_two_numbers, and division_two_numbers with two Parameters
          */
         Javaconsole myobj = new Javaconsole();
-        myobj.sum(a, b);
-        myobj.minus(a, b);
-        myobj.divide(a, b);
-        myobj.multiply(a, b);
+        myobj.sum(x, y);
+        myobj.minus(x, y);
+        myobj.divide(x, y);
+        myobj.multiply(x, y);
 
         /* Display sum, Subtraction, Multiplication, and Division of two numbers */
              sc.close();
-    
+            
 	  }
 }
